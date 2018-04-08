65273125756c3a21913d05b87a829f362a968ddb
fengzhaokang <670398931@qq.com>
2018-04-08 11:59:21 +0800

    first commit

---
+++ src/main/java/com/ym/demo1/Demo1Application.java
@@ -0,0 +1,21 @@
+package com.ym.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+	@RequestMapping("/")
+	public String toIndex(){
+		return "hello!";
+	}
+}
